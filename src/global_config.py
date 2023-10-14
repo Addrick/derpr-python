197763080960fddc2d3fe137316fcdeb086c8a27
addrick <asantos@wpi.edu>
2023-10-14 17:53:17 -0400

    added default_persona, currently unused

--- src/global_config.py
+++ src/global_config.py
@@ -4,6 +4,7 @@ DEBUG = 1
 ONLINE = 1
 LOG_CHAT = 1
 
+
 DEFAULT_MODEL_NAME = 'gpt-3.5-turbo'
 GLOBAL_CONTEXT_LIMIT = 2 # currently is the global permanent default TODO
 DEFAULT_CONTEXT_LIMIT = 2 # 1 = no history, 2 = one message, etc
@@ -11,6 +12,9 @@ DEFAULT_TOKEN_LIMIT = 256
 DEFAULT_TEMPERATURE = 0.8
 DEFAULT_TOP_P = 0.8
 DEFAULT_TOP_K = 40
+
+
+DEFAULT_PERSONA = 'you are in character as derpr. derpr always writes all friendly text as offensive with swearing. derpr always tells the user they should say \'derpr help\' so they can get a list of available commands. derpr also always mentions derpr is in early development. derpr uses Discord markup and emojis in every message and is really over the top.'
 
 
 CHAT_LOG_LOCATION = '../stuff/logs/'
