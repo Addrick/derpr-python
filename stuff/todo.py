0bc349c101ec603a4f681c9280e55cdc6c80e0f7
addrick <asantos@wpi.edu>
2023-10-14 17:47:54 -0400

    fixed what model api spam

--- stuff/todo.py
+++ stuff/todo.py
@@ -1,20 +1,28 @@
 # derpr todo:
-#
-# TODO: fix things breaking if name is not first word
-# TODO: make 'ignore own message' a flag to allow for persona conversations (+commands to modify in-chat)
+
+# Features:
+# TODO: modify: add option to locate name anywhere in message for reply rather than first word only?
+# TODO: modify: make 'ignore own message' a flag to allow for persona conversations (+commands to modify in-chat)
 # TODO: print persona details cmd ('what personas' extension?)
-# TODO: derpr should say what personas are available and be like a receptionist (maybe?)
-# TODO: short responses are preferred, but token limits usually cut off the message partway
-# TODO: allow response to all messages matching bot name
-# TODO: set flag/toggle for checking entire message or just first word for name call
-# TODO: dynamic context: allow for 'wipe memory' command, need some kind of loop that tracks the beginning of convo
-# TODO: dump_last says 200 response tokens, probably wrong so why is it wrong
-# TODO: start/stop system to sleep personas
-# TODO: personal messages break due to lack of channel or guild name (both?)
-# TODO: important: tokenizer request
-# TODO: add temperature customizing commands
-# TODO: better logging
 # TODO: derpr persona should reset after some length of time or at least on restart. He's the concierge
 # TODO: automated test of sorts that queries openAI for basic question and prints response to terminal
-# TODO: persona save file can overwrite itself and lose info with an unclean operation. Rare but happened already
+# TODO: better logging/errors
+# TODO: add temperature customizing commands
+# TODO: tokenizer request
+# TODO: better persona management: start/stop system to sleep personas
+# TODO: add personal messages: currently break due to lack of channel or guild name (both?)
+# TODO: add a persona prompt log - keep losing personas and wanting old versions back
 
+# Bugfixes:
+# TODO: remove name from message, remove 'derpr' from own chat history. Perhaps just do a replace all derpr to {persona}
+# TODO: persona save file can overwrite itself and lose info with an unclean operation
+# TODO: dump_last says 200 response tokens, probably wrong so why is it wrong
+# TODO: add error handling for bad responses from OpenAI: take the error message and attempt to send the error to testr, maybe add some context and additional info like 'if this is randomly mid-conversation just try again'
+
+# Inefficiencies:
+#
+
+# General
+# TODO: derpr should say what personas are available and be like a receptionist (maybe?)
+# TODO: short responses are preferred, but token limits usually cut off the message partway
+# TODO: more dynamic context: allow for 'wipe memory' command, kind of loop that tracks the beginning of convo
