dae6956fa05e57bccf094297f4071cd591b4e58e
addrick <asantos@wpi.edu>
2024-03-17 03:22:38 -0400

    set up proper logging

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from src.chat_system import ChatSystem
 from src.message_handler import *
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 intents = discord.Intents.all()
